3eabeb00b7742232c12e0196bb6322cab612cb90
Suraj Swarnapuri <suraj.swarnapuri@wexinc.com>
2025-09-16 15:30:51 -0500

    add user entity model

---
+++ internal/domain/user/entity.go
@@ -0,0 +1,9 @@
+package user
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+}

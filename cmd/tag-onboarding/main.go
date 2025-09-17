cdaddd21e681b24cb2cfab65b849e21bff6e411d
Suraj Swarnapuri <suraj.swarnapuri@wexinc.com>
2025-09-17 13:30:50 -0500

    link mongo to server

--- cmd/tag-onboarding/main.go
+++ cmd/tag-onboarding/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	userApplication "github.com/surajswarnapuri/ps-tag-onboarding-go/internal/application/user"
 	userEntity "github.com/surajswarnapuri/ps-tag-onboarding-go/internal/domain/user"
 	userInfra "github.com/surajswarnapuri/ps-tag-onboarding-go/internal/infrastructure/persistence/in-memory"
+	"github.com/surajswarnapuri/ps-tag-onboarding-go/internal/infrastructure/persistence/mongodb"
 	"github.com/surajswarnapuri/ps-tag-onboarding-go/internal/interface/middleware"
 	userInterface "github.com/surajswarnapuri/ps-tag-onboarding-go/internal/interface/user"
 )
 
 func main() {
-	userRepository := userInfra.NewRepository()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	uri := os.Getenv("MONGO_URI")
+	var userRepository userEntity.Repository
+
+	collection := os.Getenv("MONGO_COLLECTION")
+	if collection == "" {
+		log.Default().Println("MONGO_COLLECTION is not set, defaulting to user")
+		collection = "user"
+	}
+	client, err := mongodb.NewMongoDBClient(ctx, uri, collection)
+	if err != nil {
+		log.Default().Printf("Failed to create MongoDB client: %v\n Defaulting to inmemory storage", err)
+		userRepository = userInfra.NewRepository()
+	} else {
+
+		userRepository = mongodb.NewRepository(client)
+	}
+
 	userValidationService := userEntity.NewValidationService()
 	userService := userApplication.NewService(userValidationService, userRepository)
 	userHandler := userInterface.NewHandler(userService)

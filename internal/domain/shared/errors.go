bcee965b8b3e1e56708945fc41f5667eb39e6c21
Suraj Swarnapuri <suraj.swarnapuri@wexinc.com>
2025-09-16 19:51:56 -0500

    remove wrapped error

--- internal/domain/shared/errors.go
+++ internal/domain/shared/errors.go
@@ -1,8 +1,6 @@
 // Package shared contains shared code for the domain layer
 package shared
 
-import "fmt"
-
 type ValidationError struct {
 	Code    string
 	Message string
@@ -10,24 +8,4 @@ type ValidationError struct {
 
 func (e ValidationError) Error() string {
 	return e.Message
-}
-
-type WrappedError struct {
-	Message string
-	Err     error
-}
-
-func (e WrappedError) Error() string {
-	return fmt.Sprintf("%s: %v", e.Message, e.Err)
-}
-
-func NewWrappedError(message string, err error) WrappedError {
-	return WrappedError{
-		Message: message,
-		Err:     err,
-	}
-}
-
-func (e WrappedError) Unwrap() error {
-	return e.Err
 }
